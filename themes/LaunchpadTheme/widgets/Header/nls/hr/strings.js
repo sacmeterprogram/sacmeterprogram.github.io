f878733f7945618d835770235e2ef808a37c025e
Noah Taylor <ntaylor@carollo.com>
2017-02-15 22:57:15 -0700

    Refresh

--- themes/LaunchpadTheme/widgets/Header/nls/hr/strings.js
+++
@@ -1,8 +0,0 @@
-define({
-  "_widgetLabel": "Zaglavlje",
-  "signin": "Prijava",
-  "signout": "Odjava",
-  "about": "Informacije",
-  "signInTo": "Prijavi se u",
-  "cantSignOutTip": "Funkcija nije dostupna u načinu pretpregleda."
-});
